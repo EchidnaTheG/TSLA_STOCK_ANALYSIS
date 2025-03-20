855f8654cbd2067b82a416bed0f12481ba91322a
Echidna <176174870+EchidnaTheG@users.noreply.github.com>
2025-03-20 02:16:51 -0300

    Plotted and Calculated initial SMA_50 and SMA_200

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import yfinance as yf 
 import matplotlib.pyplot as plt
+import pandas as pd
 #We first import yfinance to get the data \n#! we will also now import matplotlib (this was added after doing data analisis)
 
 #Here, we look for Tesla information and later get a history of the stock, we set the period to be 6mo
@@ -24,7 +25,7 @@ def show_basic_stats_tsl():
 
 #Here we call all the 3 functions in a single print() and the final output sort of looks like a report
 
-print(f"{show_head_tsl(5)}\n{check_missing_values()}\n{show_basic_stats_tsl()}")
+#!!!print(f"{show_head_tsl(5)}\n{check_missing_values()}\n{show_basic_stats_tsl()}") /n#2 This was made into a comment since this is not needed at the moment
 
 #Lets Try to plot the data, the X parameter axis will be the date, the Y parameter axis is the closing price
 def access_closing_price_with_date(Ticker):
@@ -39,4 +40,35 @@ def access_closing_price_with_date(Ticker):
     plt.savefig('tesla_stock_plot.png')  # Save to file instead of showing
     plt.close()
 
-access_closing_price_with_date(tsla_history)
+#!!!access_closing_price_with_date(tsla_history) /n#2 This was made into a comment since this was already plotted, no need to do it every runtime
+
+def calculation_moving_averages_SMA_50(Ticker):
+    Ticker["SMA_50"] = Ticker.Close.rolling(window=50).mean()
+    plt.figure(figsize=(12, 6))  
+    plt.plot(Ticker.index, Ticker.SMA_50)
+    plt.title(f'{Ticker} Stock SMA_50 Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Price (USD)')
+    plt.grid(True)
+    plt.savefig('tesla_stock_SMA_50_plot.png')  
+    plt.close()
+ 
+
+
+#For this to work, we need TSLA data that goes further than 6 months, which is why another Ticker for TSLA will be created, 1year
+tsla_history_1y= tsla.history(period="1y")
+
+def calculation_moving_averages_SMA_200(Ticker):
+    if len(Ticker) >= 200:
+        Ticker["SMA_200"] = Ticker.Close.rolling(window=200).mean()
+        plt.figure(figsize=(12, 6))  
+        plt.plot(Ticker.index, Ticker.SMA_200)
+        plt.title('TSLA Stock SMA_200 Prices')
+        plt.xlabel('Date')
+        plt.ylabel('Price (USD)')
+        plt.grid(True)
+        plt.savefig('tesla_stock_SMA_200_plot.png')  
+        plt.close()
+    else:
+        raise ValueError("Not Enough Data for SMA_200")
+calculation_moving_averages_SMA_200(tsla_history_1y)
