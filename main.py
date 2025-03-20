878af13cc9b287dc9a0d93e58601ef092edb6e7a
Echidna <ej.cruz.sant@gmail.com>
2025-03-20 02:34:56 -0300

    Added the SMA Overlayed Plot, fixed some comments for ease of use

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def show_basic_stats_tsl():
 
 #Here we call all the 3 functions in a single print() and the final output sort of looks like a report
 
-#!!!print(f"{show_head_tsl(5)}\n{check_missing_values()}\n{show_basic_stats_tsl()}") /n#2 This was made into a comment since this is not needed at the moment
+#!!!print(f"{show_head_tsl(5)}\n{check_missing_values()}\n{show_basic_stats_tsl()}") #2 This was made into a comment since this is not needed at the moment
 
 #Lets Try to plot the data, the X parameter axis will be the date, the Y parameter axis is the closing price
 def access_closing_price_with_date(Ticker):
@@ -40,23 +40,26 @@ def access_closing_price_with_date(Ticker):
     plt.savefig('tesla_stock_plot.png')  # Save to file instead of showing
     plt.close()
 
-#!!!access_closing_price_with_date(tsla_history) /n#2 This was made into a comment since this was already plotted, no need to do it every runtime
+#!!!access_closing_price_with_date(tsla_history) #2 This was made into a comment since this was already plotted, no need to do it every runtime /n#3 In Order To standardize everything, the  SMA functions will use 1y 
 
 def calculation_moving_averages_SMA_50(Ticker):
-    Ticker["SMA_50"] = Ticker.Close.rolling(window=50).mean()
-    plt.figure(figsize=(12, 6))  
-    plt.plot(Ticker.index, Ticker.SMA_50)
-    plt.title(f'{Ticker} Stock SMA_50 Prices')
-    plt.xlabel('Date')
-    plt.ylabel('Price (USD)')
-    plt.grid(True)
-    plt.savefig('tesla_stock_SMA_50_plot.png')  
-    plt.close()
- 
+    if len(Ticker) >= 50:
+        Ticker["SMA_50"] = Ticker.Close.rolling(window=50).mean()
+        plt.figure(figsize=(12, 6))  
+        plt.plot(Ticker.index, Ticker.SMA_50)
+        plt.title(f'TSLA Stock SMA_50 Prices')
+        plt.xlabel('Date')
+        plt.ylabel('Price (USD)')
+        plt.grid(True)
+        plt.savefig('tesla_stock_SMA_50_plot.png')  
+        plt.close()
+    else:
+        raise ValueError("Not Enough Data for SMA_50")
 
 
 #For this to work, we need TSLA data that goes further than 6 months, which is why another Ticker for TSLA will be created, 1year
 tsla_history_1y= tsla.history(period="1y")
+#We are calculating averages, something used as sell or buy signals, 50 is short, 200 is long
 
 def calculation_moving_averages_SMA_200(Ticker):
     if len(Ticker) >= 200:
@@ -71,4 +74,26 @@ def calculation_moving_averages_SMA_200(Ticker):
         plt.close()
     else:
         raise ValueError("Not Enough Data for SMA_200")
-calculation_moving_averages_SMA_200(tsla_history_1y)
+    
+#!!!calculation_moving_averages_SMA_200(tsla_history_1y) #3 This was made into a comment since this was already plotted
+#!!!calculation_moving_averages_SMA_50(tsla_history_1y) #3 This was made into a comment since this was already plotted
+#Overalying all on a single plot
+def overlaying_SMA(Ticker):
+    if len(Ticker) >= 200:
+        Ticker["SMA_50"] = Ticker.Close.rolling(window=50).mean()
+        Ticker["SMA_200"] = Ticker.Close.rolling(window=200).mean()
+        plt.figure(figsize=(12, 6))  
+        plt.plot(Ticker.index, Ticker.SMA_50,label="50-Day SMA", color="blue")
+        plt.plot(Ticker.index, Ticker.SMA_200, label="200-Day SMA", color="orange")
+        plt.plot(Ticker.index, Ticker.Close,label="Closing Price", color="red")
+        plt.title('TSLA Stock_Overlaying_SMA')
+        plt.legend()
+        plt.xlabel('Date')
+        plt.ylabel('Price (USD)')
+        plt.grid(True)
+        plt.savefig('tesla_stock_Overlaying_SMA_plot.png')  
+        plt.close()
+    else:
+        raise ValueError("Not Enough Data for Overlaying_SMA")
+
+#!!! overlaying_SMA(tsla_history_1y) #4 Done already! no need to repeat at runtime
